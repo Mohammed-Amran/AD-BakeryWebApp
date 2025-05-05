664e4239d1b6b95f4e1604c5275ed2b3f4604119
Mohammed-Amran <mohammedamran377@gmail.com>
2025-05-05 22:56:30 +0300

    LogoutController controller added

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import DAO.DaoUsers;
+import dao.DaoUsers;
 
 @WebServlet("/loginController")
 public class LoginController extends HttpServlet {
